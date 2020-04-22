4230a37c3c28a7b546c5a7e056695b5b1acbd517
ToxicOptimism-ZY <64067075+ToxicOptimism-ZY@users.noreply.github.com>
2020-04-22 16:09:04 +0800

    Finished Question 3

--- Question3.java
+++ Question3.java
@@ -9,8 +9,19 @@ public class Question3
      * 
      * Hint: in.nextInt() to read integer
      */
-     
-    Scanner in = new Scanner(System.in);
     
+    //Declare Object
+    Scanner in = new Scanner(System.in);
+
+    //Get Input
+    System.out.print("Please input an integer: ");
+    int number = in.nextInt();
+    in.close(); //To close scanner to limit resource leak once all neccessary input has been obtained.
+
+    //Calculation
+    int numSqr = number * number;
+
+    //Display Output
+    System.out.println("The number squared is: " + numSqr);
   }
 }
