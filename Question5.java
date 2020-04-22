0e2231927f5300972a0050bda4fb9aeece440bb6
ToxicOptimism-ZY <64067075+ToxicOptimism-ZY@users.noreply.github.com>
2020-04-22 18:21:14 +0800

    Finished Question 5
    Tried doing it in a Map and HashMap way (bcus they are similiar to c#'s dictionaries)

--- Question5.java
+++ Question5.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+import java.util.HashMap; 
 import java.util.Scanner;
 
 public class Question5
@@ -26,7 +28,43 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
+     //Declare object
     Scanner in = new Scanner(System.in);
+    Map<Integer, Integer> integerCounterList = new HashMap<>(); //For storing integer and frequency respectively
+
+    //Get input
+    System.out.print("Please input the number of integers that you will be entering: ");
+    int num = in.nextInt();
+
+    //Loop num to get the set of integers
+    for(int i = 0; i < num; i++) 
+    {
+      System.out.print("Please input the " + (i+1) + "/" + num + " integer of integers that you will be entering: ");
+      int integerInput = in.nextInt();
+
+      if (integerCounterList.containsKey(integerInput)) //If integerInput already in the counter list
+      {
+        //Replace value of existing key with +1
+        integerCounterList.replace(integerInput,integerCounterList.get(integerInput) + 1);
+      }
+      else {
+        //Insert key-value pair into the hashmap
+        integerCounterList.put(integerInput,1);
+      }
+    }
+    in.close(); //No further input required, close scanner to limit resource leak
+
+    Map.Entry<Integer, Integer> highestModeEntry = null; //Need a null value for to insert the first entry
+    for(Map.Entry<Integer, Integer> current : integerCounterList.entrySet()) //.entrySet returns all entries.
+    {
+      if (highestModeEntry == null || (current.getValue() > highestModeEntry.getValue()) )  //Null check or if current entry has a higher frequency.
+      {
+        highestModeEntry = current; //replace highestMode
+      }
+      //else do no changes
+    }
+    
+    System.out.println("Highest Mode Integer: " + highestModeEntry.getKey());
     
   }
 }
